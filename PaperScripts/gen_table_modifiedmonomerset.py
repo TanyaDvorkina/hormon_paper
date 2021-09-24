559ab9bb16a390fc8b83a95fbadadd72b0904548
Tatiana Dvorkina <tanunia@Tatianas-MacBook-Pro.local>
2021-09-24 13:00:49 +0300

    Add monomer comparison

--- PaperScripts/gen_table_modifiedmonomerset.py
+++ PaperScripts/gen_table_modifiedmonomerset.py
@@ -10,14 +10,13 @@ for cen in cens:
         ln2 = fin.readline().strip().split(",")
         mp = {}
         for i in range(len(ln1)):
-            mp[ln1[i]] = ln2[i] 
+            mp[ln1[i]] = ln2[i]
     mn, mn_plus = mp["#Init mon"], mp["#Final mon"]
     merge, split = mp["#Merge"], mp["#Split"]
     sqmean, dbindex = "{:.2f}".format(float(mp["Final SqMean"])), "{:.2f}".format(float(mp["Final DBIndex"]))
 
     with open(os.path.join("./cen" + cen, "MonoRunRaw", "Monorunscnt.csv"), "r") as fin:
         edges = fin.readline().strip().split()[-1]
-        monoruns = fin.readline().strip().split()[-1]
-    res.append("\t".join([cen, mn + "/" + mn_plus, edges + "/" + monoruns, merge + "/" + split, sqmean, dbindex]))
+    res.append("\t".join([cen, mn + "/" + mn_plus, edges, merge + "/" + split, sqmean, dbindex]))
 
 print("\n".join(res))
